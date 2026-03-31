5e5aa1ab8a2cf2dd15159e13a7b5626f63762afd
Tamires Cordeiro <mirescordeiro@gmail.com>
2026-03-31 07:37:41 -0300

    feat(home): usa componente card

--- p1-pweb/src/app/interfaces/index.ts
+++ p1-pweb/src/app/interfaces/index.ts
@@ -1,2 +1,3 @@
 export { ColecaoDetalhe } from './colecao-detalhe.interface'
 export { HomeResponse } from './home.interface'
+export { Produto } from './produto.interface'
--- p1-pweb/src/app/pages/home/home.component.ts
+++ p1-pweb/src/app/pages/home/home.component.ts
@@ -1,20 +1,22 @@
 import { Component, inject } from '@angular/core';
-import { AsyncPipe, CurrencyPipe } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { Observable, map } from 'rxjs';
 
 import { HomeService } from '../../services';
 import { ColecaoDetalhe } from '../../interfaces';
+import { CardComponent } from '../../shared/components';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AsyncPipe, CurrencyPipe],
+  imports: [AsyncPipe, CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   private homeService = inject(HomeService);
-  public homeDestaque$: Observable<ColecaoDetalhe> = this.homeService.getHome().pipe(
+
+  public colecao$: Observable<ColecaoDetalhe> = this.homeService.getHome().pipe(
     map(res => res.colecaoDestaque)
   );
 }
