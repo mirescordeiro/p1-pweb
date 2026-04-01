3b185117ea91c7f01bc2c1e7725737d6f6028572
Tamires Cordeiro <mirescordeiro@gmail.com>
2026-03-31 23:38:23 -0300

    feat(cesta): cria componente de cesta

---
+++ p1-pweb/src/app/pages/cesta/cesta.component.ts
@@ -0,0 +1,32 @@
+import { Component, inject } from '@angular/core';
+import { AsyncPipe, CurrencyPipe } from '@angular/common';
+
+import { CestaService } from '../../services';
+import { CardComponent } from '../../shared/components';
+import { ItemCesta } from '../../interfaces';
+
+@Component({
+  selector: 'app-cesta',
+  standalone: true,
+  imports: [AsyncPipe, CurrencyPipe, CardComponent],
+  templateUrl: './cesta.component.html',
+  styleUrl: './cesta.component.scss',
+})
+export class CestaComponent {
+  private cestaService = inject(CestaService);
+
+  public cesta$ = this.cestaService.cesta$;
+  public total$ = this.cestaService.precoTotal$;
+
+  public adicionarNaCesta(item: ItemCesta): void {
+    this.cestaService.adicionarItem(item);
+  }
+
+  public removerDaCesta(item: ItemCesta): void {
+    this.cestaService.removerItem(item);
+  }
+
+  public limparSacola(): void {
+    this.cestaService.limpar();
+  }
+}
