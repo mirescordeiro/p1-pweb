97ddafd2c0207f4e9aa63bb551f9e6d1b7d31023
Tamires Cordeiro <mirescordeiro@gmail.com>
2026-03-31 23:43:24 -0300

    feat(produto): redireciona para cesta

--- p1-pweb/src/app/pages/produto/produto.component.spec.ts
+++ p1-pweb/src/app/pages/produto/produto.component.spec.ts
@@ -1,17 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { ProdutoComponent } from './produto.component';
+import { CestaService } from '../../services';
+import { GetProduto } from '../../mocks';
+import { of } from 'rxjs';
 
 describe('ProdutoComponent', () => {
   let component: ProdutoComponent;
   let fixture: ComponentFixture<ProdutoComponent>;
+  const activeRouteStub = {
+    paramMap: of({
+      get: (key: string) => 'sap-01',
+    }),
+  };
+  const cestaSpy = jasmine.createSpyObj('CestaService', ['adicionarItem']);
+  const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProdutoComponent]
-    })
-    .compileComponents();
-    
+      imports: [ProdutoComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: activeRouteStub },
+        { provide: CestaService, useValue: cestaSpy },
+        { provide: Router, useValue: routerSpy },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ProdutoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +34,14 @@ describe('ProdutoComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve chamar o router e cesta ao adicionarNaCesta', () => {
+    component.tamanhoSelecionado = 40;
+
+    component.adicionarNaCesta(GetProduto);
+
+    expect(cestaSpy.adicionarItem).toHaveBeenCalled();
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['cesta']);
   });
 });
