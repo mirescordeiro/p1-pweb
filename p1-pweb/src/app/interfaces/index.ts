1b2973bef979c2bc444964965e47775c1ef4ca1b
Tamires Cordeiro <mirescordeiro@gmail.com>
2026-04-03 00:59:23 -0300

    feat(services): cria servico de cadastro

--- p1-pweb/src/app/interfaces/index.ts
+++ p1-pweb/src/app/interfaces/index.ts
@@ -2,4 +2,8 @@ export { ColecaoDetalhe } from './colecao-detalhe.interface';
 export { HomeResponse } from './home.interface';
 export { ItemCesta } from './item-cesta.interface';
 export { Produto } from './produto.interface';
-export { LoginResponse } from './login.interface';
+
+export { LoginResponse } from './responses/login.interface';
+export { CadastroResponse } from './responses/cadastro.interface';
+
+export { CadastroRequest } from './requests/cadastro.request.interface';
--- p1-pweb/src/app/services/index.ts
+++ p1-pweb/src/app/services/index.ts
@@ -1,3 +1,4 @@
+export { CadastroService } from './cadastro.service';
 export { CestaService } from './cesta.service';
 export { HomeService } from './home.service';
 export { LoginService } from './login.service';
