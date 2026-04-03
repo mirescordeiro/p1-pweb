5416593737bee0e42e847f3a6d65f3a0d8d3ea7d
Tamires Cordeiro <mirescordeiro@gmail.com>
2026-04-02 23:29:20 -0300

    feat(services): cria servico de login

--- p1-pweb/src/app/services/index.ts
+++ p1-pweb/src/app/services/index.ts
@@ -1,3 +1,4 @@
 export { CestaService } from './cesta.service';
 export { HomeService } from './home.service';
+export { LoginService } from './login.service';
 export { ProdutoService } from './produto.service';
